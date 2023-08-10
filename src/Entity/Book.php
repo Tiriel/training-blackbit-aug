e121379dfd9acb4cd63a14c5839fc0ae7b147920
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-08-10 10:49:18 +0200

    Security - form login

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -46,6 +46,9 @@ class Book
     )]
     private Collection $comments;
 
+    #[ORM\ManyToOne]
+    private ?User $createdBy = null;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -166,6 +169,18 @@ class Book
                 $comment->setBook(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?User
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?User $createdBy): static
+    {
+        $this->createdBy = $createdBy;
 
         return $this;
     }
