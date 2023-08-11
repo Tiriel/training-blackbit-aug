22883f8e9f8316ac59c732e6d544e2b0444c35ea
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-08-11 13:39:27 +0200

    Test doubles

--- tests/Book/BookManagerTest.php
+++ tests/Book/BookManagerTest.php
@@ -5,23 +5,35 @@ namespace App\Tests\Book;
 use App\Book\BookManager;
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Bundle\SecurityBundle\Security;
 
-class BookManagerTest extends KernelTestCase
+class BookManagerTest extends TestCase
 {
     /**
      * @group unit
      */
     public function testFindByTitleFindsBookByPartialMatch(): void
     {
-        $repository = static::getContainer()->get(BookRepository::class);
-        $security = static::getContainer()->get('security.helper');
+        $book = (new Book())
+            ->setTitle("Le Seigneur des anneaux T1: La Fraternité de l’anneau");
+
+        $repository = $this->getMockBuilder(BookRepository::class)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
+            ->onlyMethods(['findByApproxTitle'])
+            ->getMock();
+        $repository->method('findByApproxTitle')->willReturn($book);
+
+        $security = $this->createMock(Security::class);
         $manager = new BookManager($repository, 20, $security);
 
         $lotrBook = $manager->findByTitle('Fraternité');
 
-        $this->assertNotNull($lotrBook->getId());
+        $this->assertSame($lotrBook, $book);
         $this->assertSame("Le Seigneur des anneaux T1: La Fraternité de l’anneau", $lotrBook->getTitle());
     }
 }
